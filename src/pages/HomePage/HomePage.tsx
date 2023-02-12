6cd64e8bb866616ecf842aebe1be00cb244a9d57
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-02-11 19:36:03 -0500

    home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,9 @@ import homePageImg from "../../assets/homePageImg.jpg";
 import first from "../../assets/course/1.jpg";
 import second from "../../assets/course/2.jpg";
 import third from "../../assets/course/3.jpg";
+import firstExp from "../../assets/experience/first.jpg";
+import secondExp from "../../assets/experience/second.png";
+import thirdExp from "../../assets/experience/third.png";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
@@ -11,18 +14,41 @@ const HomePage = () => {
   const cardData = [
     {
       image: first,
+      title: "Management",
       data: "Reinvesting the way to study",
       coach: "John White",
     },
     {
       image: second,
+      title: "Literature",
       data: "Arts and Culture are te way of study",
       coach: "David Truman",
     },
     {
       image: third,
+      title: "Life & Mental Health",
       data: "Science and Technology of tomorrow",
       coach: "Simon Lanback",
+    },
+  ];
+  const experienceData = [
+    {
+      image: firstExp,
+      title: "Enroll",
+      data: "Become a student of the kadenze.com community and enjoy access to the courses in our catalog, and much more.",
+      buttonText: "Register Here",
+    },
+    {
+      image: secondExp,
+      title: "A Global Classroom",
+      data: "Collaborate with your peers, showcase your work, and learn on your own schedule with our easy-to-use interactive virtual learning environment.",
+      buttonText: "More Features",
+    },
+    {
+      image: thirdExp,
+      title: "College Credit",
+      data: "Take your education to the next level by receiving college credit on select courses offered by our network of distinguished institutional partners.",
+      buttonText: "Explore Options",
     },
   ];
 
@@ -88,7 +114,7 @@ const HomePage = () => {
               >
                 <Card.Img variant="top" src={element.image} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>{element.title}</Card.Title>
                   <Card.Text>{element.data}</Card.Text>
                   <Button variant="primary">Select Course</Button>
                 </Card.Body>
@@ -96,11 +122,42 @@ const HomePage = () => {
             );
           })}
         </div>
-        <hr />
+        <hr style={{ marginInline: "100px" }} />
       </div>
       <div className="homePageContainer__experience">
         <h1 className="homePageContainer__experience__title">THE EXPERIENCE</h1>
-        <div></div>
+        <div className="homePageContainer__experience__postContainer">
+          {experienceData.map((element, index: number) => {
+            return (
+              <div
+                className="homePageContainer__experience__postContainer__postCard"
+                key={index}
+              >
+                <img
+                  className="homePageContainer__experience__postContainer__postCard__image"
+                  src={element.image}
+                  alt={"postCard"}
+                />
+                <p className="homePageContainer__experience__postContainer__postCard__title">
+                  {element.title}
+                </p>
+                <p className="homePageContainer__experience__postContainer__postCard__content">
+                  {element.data}
+                </p>
+                <button className="homePageContainer__experience__postContainer__postCard__button">
+                  {element.buttonText}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <hr style={{ marginInline: "300px" }} />
+        <p className="homePageContainer__experience__titleBottom">
+          FROM THE COMMUNITY
+        </p>
+        <p className="homePageContainer__experience__subTextBottom">
+          A community of learners growing together for over 7 years
+        </p>
       </div>
     </div>
   );
