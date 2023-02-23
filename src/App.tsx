38edff737ee25c03f62cf67d67df32231477a705
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-02-23 15:39:25 -0500

    ading redirection and gooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { lazy } from "react";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegPage from "./pages/RegPage/RegPage";
 import DashBoard from "./pages/DashBoard/DashBoard";
@@ -11,17 +11,45 @@ const AboutUs = lazy(() => import("./pages/AboutUs/AboutUs"));
 
 function App() {
   const location = useLocation();
+  const isAuthenticated = localStorage.getItem("bwUser");
   return (
     <>
       <div className="App">
         <TransitionGroup>
           <CSSTransition key={location.key} classNames="fade" timeout={300}>
             <Routes>
+              <Route
+                path="/Registration"
+                element={
+                  isAuthenticated ? (
+                    <Navigate replace to="/Dashboard" />
+                  ) : (
+                    <RegPage />
+                  )
+                }
+              />
+              <Route
+                path="/Login"
+                element={
+                  isAuthenticated ? (
+                    <Navigate replace to="/Dashboard" />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+              <Route
+                path="/Dashboard"
+                element={
+                  isAuthenticated ? (
+                    <DashBoard />
+                  ) : (
+                    <Navigate replace to="/LogIn" />
+                  )
+                }
+              />
               <Route path="/Home" element={<HomePage />} />
-              <Route path="/Login" element={<LoginPage />} />
               <Route path="/aboutUs" element={<AboutUs />} />
-              <Route path="/Registration" element={<RegPage />} />
-              <Route path="/Dashboard" element={<DashBoard />} />
             </Routes>
           </CSSTransition>
         </TransitionGroup>
