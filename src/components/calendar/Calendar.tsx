1bbfb8d2e198287f6007a9c9113e44895c292f9f
Sai Prsanna Kumar Kommuri <skommuri@iu.edu>
2023-03-01 20:28:28 -0500

    calendar sidebar navbar adjustments

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,49 +1,49 @@
-// import FullCalendar from "@fullcalendar/react";
-// import dayGridPlugin from "@fullcalendar/daygrid";
-// import timeGridPlugin from "@fullcalendar/timegrid";
-// import interactionPlugin from "@fullcalendar/interaction";
-// import React from "react";
-
-// export default function Calendar() {
-//   return (
-//     <>
-//       <div className="demo-app">
-//         <div className="demo-app-main">
-//           <FullCalendar
-//             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-//             headerToolbar={{
-//               left: "prev,next today",
-//               center: "title",
-//               right: "dayGridMonth,timeGridWeek,timeGridDay",
-//             }}
-//             initialView="dayGridMonth"
-//             editable={true}
-//             selectable={true}
-//             selectMirror={true}
-//             dayMaxEvents={true}
-//             //initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-//             //select={this.handleDateSelect}
-//             //eventContent={renderEventContent} // custom render function
-//             //eventClick={this.handleEventClick}
-//             //eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-//             /* you can update a remote database when these fire:
-//             eventAdd={function(){}}
-//             eventChange={function(){}}
-//             eventRemove={function(){}}
-//             */
-//           />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-
-
-
-import { Eventcalendar} from '@mobiscroll/react'; /* or import any other component */
-import '@mobiscroll/react/dist/css/mobiscroll.scss';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import React from "react";
+import "./Calendar.scss"
+export default function Calendar() {
+  return (
+    <>
+      <div className="demo-app">
+        <div className="calendar__container">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            //initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            //select={this.handleDateSelect}
+            //eventContent={renderEventContent} // custom render function
+            //eventClick={this.handleEventClick}
+            //eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+
+// import { Eventcalendar} from '@mobiscroll/react'; /* or import any other component */
+// import '@mobiscroll/react/dist/css/mobiscroll.scss';
+// import React from "react";
 
 
 // import "@mobiscroll/react/dist/css/mobiscroll.modular.scss"
@@ -62,29 +62,29 @@ import React from "react";
 //   });
 
 
-export default function Calendar() {
-    return (
-      <>
-        <div className="demo-app" style={{ width: '70%', height: '100%' }}>
-          <div className="demo-app-main" style={{ width: '109%', height: '184%' }}>
-            <Eventcalendar
+// export default function Calendar() {
+//     return (
+//       <>
+//         <div >
+//           <div className="demo-app-main" style={{  height: '90vh' }}>
+//             <Eventcalendar
               
-              data={[
-                {
-                  start: new Date(),
-                  title: "Today's event",
-                },
-                {
-                  start: new Date(2020, 11, 18, 9, 0),
-                  end: new Date(2020, 11, 20, 13, 0),
-                  title: "Multi day event",
-                },
-              ]}
-            />
-          </div>
-        </div>
+//               data={[
+//                 {
+//                   start: new Date(),
+//                   title: "Today's event",
+//                 },
+//                 {
+//                   start: new Date(2020, 11, 18, 9, 0),
+//                   end: new Date(2020, 11, 20, 13, 0),
+//                   title: "Multi day event",
+//                 },
+//               ]}
+//             />
+//           </div>
+//         </div>
       
-    </>
-);
-}
+//     </>
+// );
+// }
   
