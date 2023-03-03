6ffce2d99483ad130c099ef45a985817df2c2662
nsrirang <nsrirang@iu.edu>
2023-03-02 22:27:19 -0500

    faculty announcements

---
+++ src/reducers/IAnnouncementProps.ts
@@ -0,0 +1,7 @@
+export interface Announcement {
+    announcement_heading: string;
+    announcement_name: string;
+    announcement_subject: string;
+    announcement_description: string;
+    annoucement_date: string;
+  }
