2d5410e652cf2116e1938752ae643222c10a44f5
prasanna kumar <prasnicks@gmail.com>
2023-03-22 02:28:16 -0400

    UserAccount details fetch implementation 3/22

--- src/pages/UserAccount/UserAccount.tsx
+++ src/pages/UserAccount/UserAccount.tsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { useAppSelector } from "../../hooks";
+import "./UserAccount.scss";
 
 export default function UserAccount() {
+  const userDetails = useAppSelector((state) => state.userLoginAPI);
+  const coursesData = useAppSelector((state) => state.fetchCoursesReducer);
+
   return (
-    <>
-      <div>UserAccount</div>
-    </>
+    <div className="account-table">
+      <h1>User Information</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Firstname:</td>
+            <td>{userDetails.firstname}</td>
+          </tr>
+          <tr>
+            <td>Lastname:</td>
+            <td>{userDetails.lastname}</td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>{userDetails.email}</td>
+          </tr>
+          <tr>
+            <td>Username:</td>
+            <td>{userDetails.username}</td>
+          </tr>
+          <tr>
+            <td>Role:</td>
+            <td>{userDetails.role}</td>
+          </tr>
+          <tr>
+            <td>Courses Enrolled:</td>
+            <td>
+              <ul>
+                {coursesData.courseDetails.map((course) => (
+                  <li key={course}>{course}</li>
+                ))}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
