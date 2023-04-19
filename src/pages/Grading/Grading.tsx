072abf3ceb780dab85b50832fe22ce3cdfbc7ed5
nsrirang <nsrirang@iu.edu>
2023-04-19 12:43:55 -0400

    added the view assignment grading

--- src/pages/Grading/Grading.tsx
+++ src/pages/Grading/Grading.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Grading.scss";
 import { Alert, Button } from "react-bootstrap";
-import { useAppSelector } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { fetchFacultyGradingAssignments } from "../../services/assignmentService";
+import { modifyallSubmittedAssignments } from "../../reducers/submittedAssignments";
+import ViewAssignmentGrade from "../../components/ViewAssignmentsGrade/ViewAssignmentsGrade";
 export default function Grading() {
+  const dispatchReducer = useAppDispatch();
   const fetchCourses = useAppSelector((state) => state.fetchCoursesReducer);
+  useEffect(() => {
+    const data = fetchFacultyGradingAssignments();
+    data.then((e) => {
+      dispatchReducer(modifyallSubmittedAssignments(e));
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const [assignments, setAssignments] = useState({
+    show: false,
+    subject: "",
+  });
+
+  const changeHandler = (subject: string) => {
+    console.log("hi");
+    setAssignments({ ...assignments, subject: subject, show: true });
+  };
+
   return (
     <>
       <div>
@@ -16,13 +37,23 @@ export default function Grading() {
             <p className="h4">{element}</p>
             {fetchCourses.assignment.filter((e) => e.courseFullName === element)
               .length ? (
-              <Button variant="info">View Assignments</Button>
+              <Button variant="info" onClick={() => changeHandler(element)}>
+                View Assignments
+              </Button>
             ) : (
               <p>No assignments posted</p>
             )}
           </Alert>
         ))}
         <hr />
+      </div>
+
+      <div>
+        {assignments.show ? (
+          <ViewAssignmentGrade subjectToShow = {assignments.subject}/>
+        ) : (
+          <p>No assignments posted</p>
+        )}
       </div>
     </>
   );
