1dcfbc7affba2e311bb560929a955c871fa87083
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-04-19 11:46:17 -0400

    adding space for courses in grading

--- src/pages/Grading/Grading.tsx
+++ src/pages/Grading/Grading.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Grading.scss";
+import { Alert, Button } from "react-bootstrap";
+import { useAppSelector } from "../../hooks";
 
 export default function Grading() {
+  const fetchCourses = useAppSelector((state) => state.fetchCoursesReducer);
   return (
     <>
-      <div>Grading</div>
+      <div>
+        {fetchCourses.courseDetails.map((element, index) => (
+          <Alert
+            key={index}
+            className="m-3 d-flex flex-row justify-content-between"
+            variant="info"
+          >
+            <p>{element}</p>
+            <Button>OK</Button>
+          </Alert>
+        ))}
+      </div>
     </>
   );
 }
