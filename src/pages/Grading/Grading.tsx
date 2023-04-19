4e2fb3504d9295ce8cbb521d69f9f527cd088950
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-04-19 11:56:54 -0400

    adding button functionality

--- src/pages/Grading/Grading.tsx
+++ src/pages/Grading/Grading.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Grading.scss";
 import { Alert, Button } from "react-bootstrap";
 import { useAppSelector } from "../../hooks";
-
 export default function Grading() {
   const fetchCourses = useAppSelector((state) => state.fetchCoursesReducer);
   return (
@@ -14,10 +13,16 @@ export default function Grading() {
             className="m-3 d-flex flex-row justify-content-between"
             variant="info"
           >
-            <p>{element}</p>
-            <Button>OK</Button>
+            <p className="h4">{element}</p>
+            {fetchCourses.assignment.filter((e) => e.courseFullName === element)
+              .length ? (
+              <Button variant="info">View Assignments</Button>
+            ) : (
+              <p>No assignments posted</p>
+            )}
           </Alert>
         ))}
+        <hr />
       </div>
     </>
   );
